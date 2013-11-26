eda3a9e91131b95e7dbf03337a5ed533cac58137
ashutoshsrinet <ashutoshsrinet@users.noreply.github.com>
2013-11-26 16:34:35 +0530

    added facebook login button to footer

--- TestPHP/templates/include/footer.php
+++ TestPHP/templates/include/footer.php
@@ -1,5 +1,14 @@
       <div id="footer">
         Volunteer Ninjas &copy; 2013. All rights reserved. <a href="admin.php">Site Admin</a>
+        <?php	
+		include_once "fbaccess.php";
+		if ($user) echo '<a href="' . $logoutUrl . '">Logout</a>';
+		else echo '<a href="' . $loginUrl . '">Login</a>';
+		if ($user){
+			echo($user);
+			echo($user_info['email']);
+		}
+	?>
       </div>
 
     </div>
