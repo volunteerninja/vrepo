65dfe8f14f4f00dc909fa9baafc8ec77e95d19cc
ashutoshsrinet <ashutoshsrinet@users.noreply.github.com>
2013-11-26 16:35:34 +0530

    adding profile image from fb

--- TestPHP/templates/include/header.php
+++ TestPHP/templates/include/header.php
@@ -2,11 +2,12 @@
 <html lang="en">
   <head>
     <title><?php echo htmlspecialchars( $results['pageTitle'] )?></title>
+	<?php include_once "fbaccess.php"; ?>
     <link rel="stylesheet" type="text/css" href="style.css" />
   </head>
   <body>
     <div id="container">
 
-      <a href="." class="abc"><img id="logo" src="images/logo.jpg" alt="Volunteer Ninjas" /><img id="title" src="images/logo_txt.png" alt="Volunteer Ninjas" /></a>
+      <a href="." class="abc"><img id="logo" src="images/logo.jpg" alt="Volunteer Ninjas" /><img id="title" src="images/logo_txt.png" alt="Volunteer Ninjas" /></a><?php if($user) echo('<img src="https://graph.facebook.com/'. $user .'/picture?type=large"/>');?>
 	  <br style="clear:both"/>
 
